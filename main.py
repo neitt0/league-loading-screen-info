6a8a0c07f0db9f75e07bde054076bcf2f6191b87
Thien Nguyen <114895619+neitt0@users.noreply.github.com>
2024-04-09 00:51:51 -0400

    fixed bug with api through env

--- main.py
+++ main.py
@@ -1,48 +1,45 @@
 import requests
-import os
 from decouple import config
 
 api_key = config('RIOT_API_KEY')
 
 # USER INFO
 # CHANGE LATER
-region = 'na1'
-summonerName = 'Adrian%20Seira'
+region = 'euw1'
+summonerName = 'Spear%20Shot'
 
-# code works (idk how)
+# code works (i do kn how now)
+# Account V1
 def getPlayerId(region, summonerName, api_key):
-  playerURL = "https://" + region + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=" + api_key
+  playerURL = f"https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={api_key}"
   response = requests.get(playerURL).json()
   return response['id']
-  # return requests.get(playerURL)
 
-def createLiveGameURL(region, summonerID, api_key):
-  gameURL = "https://" + region + ".api.riotgames.com/lol/spectator/v4/active-games/by-summoner/" + summonerID + "?api_key=" + api_key
-  return requests.get(gameURL).json()['participants']
-#
-
-
-# Get Jungler in live game
-playersInLiveGame = createLiveGameURL(region, getPlayerId(region, summonerName, api_key), api_key)
-
-junglerList = []
-
-for player in playersInLiveGame:
-  if player['spell1Id'] == 11 or player['spell2Id'] == 11:
-    junglerList.append(player)
-
-# print(junglerList)
+# def createLiveGameURL(region, summonerID, api_key):
+#   gameURL = "https://" + region + ".api.riotgames.com/lol/spectator/v4/active-games/by-summoner/" + summonerID + "?api_key=" + api_key
+#   return requests.get(gameURL).json()['participants']
+# #
 
 
-# Get Champion from jungler
-championsDDragon = requests.get('https://ddragon.leagueoflegends.com/cdn/14.5.1/data/en_US/champion.json').json()['data']
+# # Get Jungler in live game
+# playersInLiveGame = createLiveGameURL(region, getPlayerId(region, summonerName, api_key), api_key)
 
-jgChamList = []
+# junglerList = []
 
-for jungler in junglerList:
-  championKey = jungler['championId']
-  for champion in championsDDragon:
-    if championKey == int(championsDDragon[champion]['key']):
-      jgChamList.append(champion)
+# for player in playersInLiveGame:
+#   if player['spell1Id'] == 11 or player['spell2Id'] == 11:
+#     junglerList.append(player)
 
-print(jgChamList)
+# # print(junglerList)
+
+
+# # Get Champion from jungler
+# championsDDragon = requests.get('https://ddragon.leagueoflegends.com/cdn/14.5.1/data/en_US/champion.json').json()['data']
+
+# jgChamList = []
+
+# for jungler in junglerList:
+#   championKey = jungler['championId']
+#   for champion in championsDDragon:
+#     if championKey == int(championsDDragon[champion]['key']):
+#       jgChamList.append(champion)
---
+++ readme.md
@@ -0,0 +1,12 @@
+Personal project using Riot's API to create an application that shows informations about the opposing characters in game.
+
+MVP
+display 2 jungler's popular pathing
+(playing as gwen) display the matchup guides for the respective character from this exel (https://docs.google.com/spreadsheets/d/1vfJg3sp1TCz1PV0AlYvOpwb1mR2iHJeb0x5mGl8r_vY/edit#gid=1724373785)
+
+
+things learned from this project
+Utilizing API / requests
+python
+env
+beautifulsoup
